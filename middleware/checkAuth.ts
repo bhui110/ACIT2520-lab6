b380ae031103ae4e678640b2bdb0fee73264117b
brandon hui <brandonlikesstuff@gmail.com>
2025-10-23 23:51:18 -0700

    added admin page

--- middleware/checkAuth.ts
+++ middleware/checkAuth.ts
@@ -15,7 +15,7 @@ export const forwardAuthenticated = (req: Request, res: Response, next: NextFunc
 }
 
 export const ensureAdmin = (req: Request, res: Response, next: NextFunction) => {
-  if (req.isAuthenticated() && req.user.role === "admin") {
+  if (req.isAuthenticated() && req.user && req.user.role === "admin") {
     return next();
   }
   res.redirect("/dashboard");
